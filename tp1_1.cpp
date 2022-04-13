4d028249106484a11d3ef50d467bbdef4ed02dff
juani981 <juanicarrizo97@gmail.com>
2022-04-12 21:58:09 -0300

    Segunda modificacion archivo tp1_1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -5,5 +5,12 @@ int main(){
     printf("Hola Mundo \n");
       int *puntero, variable=999;
 
+    puntero=&variable;
+
+    printf("Contenido del puntero: %d \n",*puntero);
+    printf("Direccion de memoria almacenada: %p \n",puntero);
+    printf("Direccion de memoria de la variable: %p \n",&variable);
+    printf("Direccion de memoria del puntero: %p \n",&puntero);
+    printf("Tamaño de la variable: %d \n",sizeof(variable));
 return 0;
 }
